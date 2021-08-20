cdd0a235e0491da67b95029a5621a29a20997a81
tak <Takkarigomba@gmail.com>
2021-08-20 09:05:47 +0100

    Struct named Lead to store info about all LEADS

---
+++ Project/Project/Leads.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+struct Lead
+{
+	string first_name;
+	string last_name;
+	string description;
+	string phone_number;
+};
+
+int main()
+{
+}
